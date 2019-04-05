d6e389e0dfa4c9bd474f98567a170f0c40474271
v.grechukhin <v.grechukhin@whatwhere.world>
2019-04-05 23:37:28 +0300

    Add important commands

--- bot/commands/info/help.py
+++ bot/commands/info/help.py
@@ -4,6 +4,6 @@ from bot.commands import BaseCommand
 class HelpCommand(BaseCommand):
 
     _COMMAND = 'help'
-    _SUCCESS_MESSAGE = 'Help message'
+    _SUCCESS_MESSAGE = 'Nobody can help you apart from yourself'
 
-    _DESCRIPTION = 'Show available commands'
+    _DESCRIPTION = 'Help'
