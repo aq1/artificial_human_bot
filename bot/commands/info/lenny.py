a229ba8f514a4ea41c4d8fae7d30da38915a6b5e
aq1 <inmosh@protonmail.ch>
2019-09-15 16:08:34 +0300

    Update lenny.py

--- bot/commands/info/lenny.py
+++ bot/commands/info/lenny.py
@@ -10,17 +10,3 @@ class LennyCommand(BaseCommand):
     _SUCCESS_MESSAGE = '( ͡° ͜ʖ ͡°)'
 
     _DESCRIPTION = 'Gives you {}'.format(_SUCCESS_MESSAGE)
-
-    def _call(self, bot, update, **kwargs):
-        if str(update.message.from_user.id) != '387116733':
-            return True
-
-        if random.randint(0, 2) == 0:
-            return True
-
-        bot.send_message(
-            update.message.chat_id,
-            text='Ой, Федя иди нафиг',
-            parse_mode=telegram.ParseMode.MARKDOWN,
-        )
-        return False
