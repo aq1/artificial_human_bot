d91cce65df171041c744674359a83ab68fda000d
v.grechukhin <v.grechukhin@whatwhere.world>
2019-10-26 12:39:02 +0300

    Task

--- manage.py
+++ manage.py
@@ -1,11 +1,22 @@
 import os
+import sys
 
 import settings
 import bot
+import tasks
 
 
 if __name__ == '__main__':
     pid_path = os.path.join(settings.PID_PATH)
     with open(pid_path, 'w') as f:
         f.write(str(os.getpid()))
-    bot.start_bot()
+
+    try:
+        arg = sys.argv[1]
+    except IndexError:
+        arg = 'start_bot'
+
+    if arg == 'start_bot':
+        bot.start_bot()
+    if arg == 'find_projects':
+        tasks.find_projects()
---
+++ tasks/__init__.py
@@ -0,0 +1,1 @@
+from tasks.find_projects import find_projects
